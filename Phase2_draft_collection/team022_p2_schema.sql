c4a9bc074b9af14ef771f8b4103a1ad5fccc3c8d
Xia, Hui <huix@gatech.edu>
2019-06-29 09:29:43 -0500

    Update team022_p2_schema.sql
    Run-able on PostgreSQL 10

--- Phase2_draft_collection/team022_p2_schema.sql
+++ Phase2_draft_collection/team022_p2_schema.sql
@@ -1,5 +1,7 @@
+DROP TABLE IF EXISTS Users, Manager, InventoryClerk, Salesperson, Customer, Person, Business, VehicleType, Manufacturer, Vehicle, VehicleColor, Recall, Vendor, Repair, Buy, Sell;
+
 -- tables for users
-CREATE TABLE User (
+CREATE TABLE Users (
   username varchar(50) NOT NULL,
   password varchar(50) NOT NULL,
   login_first_name varchar(50) NOT NULL,
@@ -11,24 +13,27 @@ CREATE TABLE Manager (
   username varchar(50) NOT NULL,
   manager_permission varchar(50) NOT NULL,
   PRIMARY KEY (username),
+  UNIQUE (manager_permission),
   FOREIGN KEY (username)
-    REFERENCES User (username)
+    REFERENCES Users (username)
 );
 
 CREATE TABLE InventoryClerk (
   username varchar(50) NOT NULL,
   inventory_clerk_permission varchar(50) NOT NULL,
   PRIMARY KEY (username),
+  UNIQUE (inventory_clerk_permission),
   FOREIGN KEY (username)
-    REFERENCES User (username)
+    REFERENCES Users (username)
 );
 
 CREATE TABLE Salesperson (
   username varchar(50) NOT NULL,
   salesperson_permission varchar(50) NOT NULL,
   PRIMARY KEY (username),
+  UNIQUE (salesperson_permission),
   FOREIGN KEY (username)
-    REFERENCES User (username)
+    REFERENCES Users (username)
 );
 
 -- tables for customer
@@ -65,41 +70,6 @@ CREATE TABLE Business (
     REFERENCES Customer (customer_id)
 );
 
--- buy and sell transactions
-CREATE TABLE Buy (
-  VIN varchar(50) NOT NULL,
-  customer_id varchar(50) NOT NULL,
-  inventory_clerk_permission varchar(50) NOT NULL,
-  purchase_date timestamp NOT NULL,
-  purchase_price decimal NOT NULL,
-  purchase_condition varchar(50) NOT NULL,
-  KBB_value decimal NOT NULL,
-  UNIQUE (VIN, inventory_clerk_permission, customer_id)
-  PRIMARY KEY (VIN),
-  FOREIGN KEY (VIN)
-    REFERENCES Vehicle (VIN),
-  FOREIGN KEY (inventory_clerk_permission)
-    REFERENCES InventoryClerk (inventory_clerk_permission),
-  FOREIGN KEY (customer_id)
-    REFERENCES Customer (customer_id)
-);
-
-CREATE TABLE Sell (
-  VIN varchar(50) NOT NULL,
-  customer_id varchar(50) NOT NULL,
-  salesperson_permission varchar(50) NOT NULL,
-  sale_date timestamp NOT NULL,
-  sale_price decimal NOT NULL,
-  UNIQUE (VIN, salesperson_permission, customer_id)
-  PRIMARY KEY (VIN),
-  FOREIGN KEY (VIN)
-    REFERENCES Vehicle (VIN),
-  FOREIGN KEY (salesperson_permission)
-    REFERENCES Salesperson (salesperson_permission),
-  FOREIGN KEY (customer_id)
-    REFERENCES Customer (customer_id)
-);
-
 -- vehicle and repair table
 CREATE TABLE VehicleType (
   type_name varchar(50) NOT NULL,
@@ -112,14 +82,14 @@ CREATE TABLE Manufacturer (
 );
 
 CREATE TABLE Vehicle (
-  VIN varchar(50) NOT NULL,
+  vin varchar(50) NOT NULL,
   vehicle_mileage int NOT NULL,
   vehicle_description varchar(250) NULL,
   model_name varchar(50) NOT NULL,
   model_year int NOT NULL,
   type_name varchar(50) NOT NULL,
   manufacturer_name varchar(50) NOT NULL,
-  PRIMARY KEY (VIN),
+  PRIMARY KEY (vin),
   FOREIGN KEY (type_name)
     REFERENCES VehicleType (type_name),
   FOREIGN KEY (manufacturer_name)
@@ -127,38 +97,85 @@ CREATE TABLE Vehicle (
 );
 
 CREATE TABLE VehicleColor (
-  VIN varchar(50) NOT NULL,
+  vin varchar(50) NOT NULL,
   vehicle_color varchar(50) NOT NULL,
-  PRIMARY KEY (VIN, vehicle_color),
-  FOREIGN KEY (VIN)
-    REFERENCES Vehicle (VIN),
+  PRIMARY KEY (vin, vehicle_color),
+  FOREIGN KEY (vin)
+    REFERENCES Vehicle (vin)
 );
 
 CREATE TABLE Recall (
   recall_manufacturer varchar(50) NOT NULL,
   recall_description varchar(250) NULL,
   NHTSA_recall_compaign_number varchar(50) NULL,
-  PRIMARY KEY (NHTSA_recall_compaign_number),
+  PRIMARY KEY (nhtsa_recall_compaign_number),
   FOREIGN KEY (recall_manufacturer)
     REFERENCES Manufacturer (manufacturer_name)
 );
 
+CREATE TABLE Vendor (
+  vendor_name varchar(50) NOT NULL,
+  vendor_phone_number int NOT NULL,
+  vendor_street varchar(50) NOT NULL,
+  vendor_city varchar(50) NOT NULL,
+  vendor_state varchar(50) NOT NULL,
+  vendor_zip int NOT NULL,
+  PRIMARY KEY (vendor_name)
+);
+
 CREATE TABLE Repair (
-  VIN varchar(50) NOT NULL,
+  vin varchar(50) NOT NULL,
   start_date timestamp NOT NULL,
   end_date timestamp NOT NULL,
   repair_status varchar(50) NOT NULL,
   repair_description varchar(250) NULL,
   vendor_name varchar(50) NOT NULL,
   repair_cost decimal NOT NULL,
-  NHTSA_recall_compaign_number varchar(50) NULL,
+  nhtsa_recall_compaign_number varchar(50) NULL,
   inventory_clerk_permission varchar(50) NOT NULL,
-  PRIMARY KEY (VIN),
-  UNIQUE (VIN, start_date),
-  FOREIGN KEY (VIN)
-    REFERENCES Vehicle (VIN),
+  PRIMARY KEY (vin),
+  UNIQUE (vin, start_date),
+  FOREIGN KEY (vin)
+    REFERENCES Vehicle (vin),
   FOREIGN KEY (vendor_name)
     REFERENCES Vendor (vendor_name),
-  FOREIGN KEY (NHTSA_recall_compaign_number)
-    REFERENCES Recall (NHTSA_recall_compaign_number)
+  FOREIGN KEY (nhtsa_recall_compaign_number)
+    REFERENCES Recall (nhtsa_recall_compaign_number)
 );
+
+-- buy and sell transactions
+CREATE TABLE Buy (
+  vin varchar(50) NOT NULL,
+  customer_id varchar(50) NOT NULL,
+  inventory_clerk_permission varchar(50) NOT NULL,
+  purchase_date timestamp NOT NULL,
+  purchase_price decimal NOT NULL,
+  purchase_condition varchar(50) NOT NULL,
+  KBB_value decimal NOT NULL,
+  UNIQUE (vin, inventory_clerk_permission, customer_id),
+  PRIMARY KEY (vin),
+  FOREIGN KEY (vin)
+    REFERENCES Vehicle (vin),
+  FOREIGN KEY (inventory_clerk_permission)
+    REFERENCES InventoryClerk (inventory_clerk_permission),
+  FOREIGN KEY (customer_id)
+    REFERENCES Customer (customer_id)
+);
+
+CREATE TABLE Sell (
+  vin varchar(50) NOT NULL,
+  customer_id varchar(50) NOT NULL,
+  salesperson_permission varchar(50) NOT NULL,
+  sale_date timestamp NOT NULL,
+  sale_price decimal NOT NULL,
+  UNIQUE (vin, salesperson_permission, customer_id),
+  PRIMARY KEY (vin),
+  FOREIGN KEY (vin)
+    REFERENCES Vehicle (vin),
+  FOREIGN KEY (salesperson_permission)
+    REFERENCES Salesperson (salesperson_permission),
+  FOREIGN KEY (customer_id)
+    REFERENCES Customer (customer_id)
+);
+
+
