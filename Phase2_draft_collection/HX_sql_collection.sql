51243ce983b711e46fcd3608b1407560ba38a1f9
Xia, Hui <huix@gatech.edu>
2019-06-30 14:16:04 -0500

    Update HX_sql_collection.sql

--- Phase2_draft_collection/HX_sql_collection.sql
+++ Phase2_draft_collection/HX_sql_collection.sql
@@ -8,11 +8,12 @@ SELECT
 	COUNT(Repair.vin)/COUNT(Repair.vendor_name) AS avg_repair_per_vehicle,
 	AVG(DATE_PART('DAY', Repair.end_date - Repair.start_date)) AS avg_time_per_repair
 FROM Repair
+WHERE Repair.repair_status = 'complete'
 GROUP BY Repair.vendor_name
 ORDER BY Repair.vendor_name;
 
 
---Monthly Report , yearly sale summary page	
+--Monthly Report, yearly sale summary page	
 SELECT 
 	COUNT(Sell.vin) AS Num_of_vehicle_sold,
 	SUM(Sell.sale_price) AS total_sale_income,
@@ -26,7 +27,7 @@ ON Sell.vin = Repair.vin
 GROUP BY Sale_year
 ORDER BY Sale_year DESC; 
 		      
---Monthly Report , monthly sale summary page		      
+--Monthly Report, monthly sale summary page		      
 SELECT 
 	COUNT(Sell.vin) AS Num_of_vehicle_sold,
 	SUM(Sell.sale_price) AS total_sale_income,
@@ -41,3 +42,18 @@ GROUP BY Sale_month
 ORDER BY Sale_month DESC;
 							     
 --Monthly Report , month sale drill down report
+
+SELECT 
+	Sell.vin,
+	Sell.salesperson_permission,
+	Sell.sale_price,
+	Sell.sale_date,
+	Salesperson.username,
+	Users.login_first_name,
+	Users.login_last_name
+FROM Sell
+JOIN Salesperson
+ON Sell.salesperson_permission = Salesperson.salesperson_permission
+JOIN Users
+ON Salesperson.username = Users.username
+WHERE LEFT(Sell.sale_date::text, 4) = '2019';
