05cca6cfc5d6e9cddcf4cb6a344be1f6b7fe33fb
Xia, Hui <huix@gatech.edu>
2019-06-30 17:18:35 -0500

    Update HX_sql_collection.sql

--- Phase2_draft_collection/HX_sql_collection.sql
+++ Phase2_draft_collection/HX_sql_collection.sql
@@ -37,8 +37,10 @@ SELECT
 	MAX(Users.login_first_name) AS top_seller_first_name,
 	MAX(Users.login_last_name) AS top_seller_last_name,
 	COUNT(Sell.vin) AS num_vehicle_sold,	
-	SUM(Sell.sale_price) AS total_sales
+	SUM(Vehicle.sale_price) AS total_sales
 FROM Sell
+JOIN Vehicle
+ON Vehicle.vin = Sell.vin
 JOIN Salesperson
 ON Sell.salesperson_permission = Salesperson.salesperson_permission
 JOIN Users
@@ -48,21 +50,23 @@ GROUP BY Salesperson.username
 ORDER BY 
 num_vehicle_sold DESC,
 total_sales DESC
-LIMIT 1;	     
+LIMIT 1;   
 		      
 --Monthly sale summary page		      
 SELECT 
 	COUNT(Sell.vin) AS Num_of_vehicle_sold,
-	SUM(Sell.sale_price) AS total_sale_income,
-	(SUM(Sell.sale_price)- SUM(Buy.purchase_price) - SUM(Repair.repair_cost)) AS net_income,
+	SUM(Vehicle.sale_price) AS total_sale_income,
+	(SUM(Vehicle.sale_price)- SUM(Buy.purchase_price) - SUM(Repair.repair_cost)) AS net_income,
 	LEFT(Sell.sale_date::text, 7) AS Sale_month
 FROM Sell
 JOIN Buy
 ON Sell.vin = Buy.vin
 JOIN Repair
 ON Sell.vin = Repair.vin
+JOIN Vehicle
+ON Sell.vin = Vehicle.vin
 GROUP BY Sale_month
-ORDER BY Sale_month DESC;
+ORDER BY Sale_month DESC; 
 							     
 
 							     
@@ -72,8 +76,10 @@ SELECT
 	MAX(Users.login_first_name) AS top_seller_first_name,
 	MAX(Users.login_last_name) AS top_seller_last_name,
 	COUNT(Sell.vin) AS num_vehicle_sold,	
-	SUM(Sell.sale_price) AS total_sales
+	SUM(Vehicle.sale_price) AS total_sales
 FROM Sell
+JOIN Vehicle
+ON Vehicle.vin = Sell.vin
 JOIN Salesperson
 ON Sell.salesperson_permission = Salesperson.salesperson_permission
 JOIN Users
