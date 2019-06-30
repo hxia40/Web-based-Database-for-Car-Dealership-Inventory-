8026f02a29acf50196175cf2b3bea60019e11747
Xia, Hui <huix@gatech.edu>
2019-06-30 13:46:46 -0500

    Update HX_sql_collection.sql

--- Phase2_draft_collection/HX_sql_collection.sql
+++ Phase2_draft_collection/HX_sql_collection.sql
@@ -10,3 +10,34 @@ SELECT
 FROM Repair
 GROUP BY Repair.vendor_name
 ORDER BY Repair.vendor_name;
+
+
+--Monthly Report , yearly sale summary page	
+SELECT 
+	COUNT(Sell.vin) AS Num_of_vehicle_sold,
+	SUM(Sell.sale_price) AS total_sale_income,
+	(SUM(Sell.sale_price)- SUM(Buy.purchase_price) - SUM(Repair.repair_cost)) AS net_income,
+	LEFT(Sell.sale_date::text, 4) AS Sale_year
+FROM Sell
+JOIN Buy
+ON Sell.vin = Buy.vin
+JOIN Repair
+ON Sell.vin = Repair.vin
+GROUP BY Sale_year
+ORDER BY Sale_year DESC; 
+		      
+--Monthly Report , monthly sale summary page		      
+SELECT 
+	COUNT(Sell.vin) AS Num_of_vehicle_sold,
+	SUM(Sell.sale_price) AS total_sale_income,
+	(SUM(Sell.sale_price)- SUM(Buy.purchase_price) - SUM(Repair.repair_cost)) AS net_income,
+	LEFT(Sell.sale_date::text, 7) AS Sale_month
+FROM Sell
+JOIN Buy
+ON Sell.vin = Buy.vin
+JOIN Repair
+ON Sell.vin = Repair.vin
+GROUP BY Sale_month
+ORDER BY Sale_month DESC;
+							     
+--Monthly Report , month sale drill down report
