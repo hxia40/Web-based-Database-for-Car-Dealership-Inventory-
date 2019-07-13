e2e3fd06ee2e465ae0039e5b3ae2cc9aab7e627b
Jinjun Liu <jliu788@gatech.edu>
2019-07-13 13:02:49 -0500

    modify header icon link

--- Phase_3/lib/header.php
+++ Phase_3/lib/header.php
@@ -3,4 +3,4 @@
 		<link rel="shortcut icon" href="img/sports-car.png" />
 		<link rel="stylesheet" type="text/css" href="css/gtonline_style.css" />
         <meta http-equiv="Content-Type" content="text/html; charset="UTF-8" />
-        <link rel="shortcut icon" type="image/png" href="img/gtonline_icon.png"/>
+        <link rel="shortcut icon" type="image/png" href="img/sports-car.png"/>
