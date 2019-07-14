2260f5ce25543d44821996c54135ea2e564d9f06
Jinjun Liu <jliu788@gatech.edu>
2019-07-13 22:36:10 -0500

    menu

--- Phase_3/lib/footer.php
+++ Phase_3/lib/footer.php
@@ -1,11 +1,11 @@
-			
-            <div id="footer"> 
-                <div class="footer">
-
-                Gatech CS6400 Team 22
-
-                <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
-				
-				</div>
-            </div>	 
+<div id="footer"> 
+    <div class="footer">
+        Gatech CS6400 Team 22
+        
+        <div>Credit:</div>
+        <div>We used some codes and materials from GTOnline template.</div>
+        <div>The cartoon car on the logo image is from: http://www.sclance.com/pngs/car-cartoon-png/view-page-2.htm . </div>
+        <div>Website icon made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>	
+    </div>
+</div>	 
 
