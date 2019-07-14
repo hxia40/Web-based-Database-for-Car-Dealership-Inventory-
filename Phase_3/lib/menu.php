ea52284d4ea87f287288963afbae3994133b0ab1
Xia, Hui <huix@gatech.edu>
2019-07-13 21:28:20 -0500

    HX- update for reports

--- Phase_3/lib/menu.php
+++ Phase_3/lib/menu.php
@@ -11,6 +11,13 @@
                     <li><a href="search_friends.php" <?php if($current_filename=='search_friends.php') echo "class='active'"; ?>>Search for Friends</a></li>  
                     <li><a href="view_requests.php" <?php if($current_filename=='view_requests.php') echo "class='active'"; ?>>View Requests</a></li>  
                     <li><a href="view_updates.php" <?php if($current_filename=='view_updates.php') echo "class='active'"; ?>>View Status Updates</a></li>  
-                    <li><a href="logout.php" <span class='glyphicon glyphicon-log-out'></span> Log Out</a></li>              
+                    <li><a href="logout.php" <span class='glyphicon glyphicon-log-out'></span> Log Out</a></li>
+<!-- 		    HX - Below are report menus -->
+		    <li><a href="view_seller_history_report.php" <?php if($current_filename=='view_seller_history_report.php') echo "class='active'"; ?>>View Seller History Report</a></li>
+                    <li><a href="view_inventory_age_report.php" <?php if($current_filename=='view_inventory_age_report.php') echo "class='active'"; ?>>View Inventory Age Report</a></li>
+                    <li><a href="view_average_time_in_inventory_report.php" <?php if($current_filename=='view_average_time_in_inventory_report.php') echo "class='active'"; ?>>View Average Time In Inventory Report</a></li>
+                    <li><a href="view_price_per_condition_report.php" <?php if($current_filename=='view_price_per_condition_report.php') echo "class='active'"; ?>>View Price Per Condition Report</a></li>
+                    <li><a href="view_repair_statistics_report.php" <?php if($current_filename=='view_repair_statistics_report.php') echo "class='active'"; ?>>View Repair Statistics Report</a></li>
+                    <li><a href="view_monthly_sales_report.php" <?php if($current_filename=='view_monthly_sales_report.php') echo "class='active'"; ?>>View Monthly Sales Report</a></li>
 				</ul>
 			</div>
