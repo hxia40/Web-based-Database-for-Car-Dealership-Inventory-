f1b9d6f918913d8a2c78aaba917b0015a8cde199
Xia, Hui <huix@gatech.edu>
2019-07-14 15:32:55 -0500

    changed text color to white

--- Phase_3/lib/footer.php
+++ Phase_3/lib/footer.php
@@ -1,11 +1,12 @@
 <div id="footer"> 
-    <div class="footer">
+    <div class="footer" style="color:white;">
         Gatech CS6400 Team 22
         
-        <div>Credit:</div>
-        <div>GTOnline template.</div>
-        <div>The cartoon car on the logo image is from <a href="http://www.sclance.com/pngs/car-cartoon-png/view-page-2.htm" title="SCLANCE">SCLANCE</a>.</div>
-        <div>Website icon made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>	
+        <div style="color:white;">Credit:</div>
+        <div style="color:white;">We used some codes and materials from GTOnline template.</div>
+        <div style="color:white;">The cartoon car on the logo image is from: http://www.sclance.com/pngs/car-cartoon-png/view-page-2.htm . </div>
+        <div style="color:white;">Website icon made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"                 title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"                 title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
     </div>
-</div>
+</div>	 
+
 
