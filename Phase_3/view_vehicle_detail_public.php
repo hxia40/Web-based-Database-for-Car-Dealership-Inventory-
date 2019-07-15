fcf9809196583fcc3165bca3bc91e97db03f7d1c
Xia, Hui <huix@gatech.edu>
2019-07-15 01:28:56 -0500

    +distinct

--- Phase_3/view_vehicle_detail_public.php
+++ Phase_3/view_vehicle_detail_public.php
@@ -3,7 +3,7 @@ include('lib/common.php');
 // written by czhang613
 
     $enteredVIN = $_GET['vin'];
-    $query = "SELECT Vehicle.vin, vehicle_mileage, vehicle_description, model_name, model_year, manufacturer_name, GROUP_CONCAT(vehicle_color SEPARATOR ', ') AS color, sale_price
+    $query = "SELECT Vehicle.vin, vehicle_mileage, vehicle_description, model_name, model_year, manufacturer_name, GROUP_CONCAT(DISTINCT vehicle_color SEPARATOR ', ') AS color, sale_price
     FROM Vehicle JOIN VehicleColor ON Vehicle.vin = VehicleColor.vin
     JOIN Repair ON Vehicle.vin = Repair.vin
     WHERE repair_status = 'completed' AND Vehicle.vin = '$enteredVIN'";
