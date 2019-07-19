441a61f0f0c7acbb5ef4b4dd141af358cbddcd85
Xie, Zijian <jason.xie@gatech.edu>
2019-07-19 10:38:09 -0400

    Update edit_buy.php

--- Phase_3/edit_buy.php
+++ Phase_3/edit_buy.php
@@ -165,12 +165,12 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
                             <tr>
                                 <td class="item_label">Purchase Condition</td>
                                 <td>
-                                    <select name="type_name">
+                                    <select name="purchase_condition">
                                         <option value='select' selected="true">Please select</option>
                                         <?php
                                             foreach($PURCHASE_CONDITION_LIST as $var) {
                                         ?>
-                                        <option value='<?php echo $var;?>'><?php echo $var;?></option>
+			                                <option value= '<?php echo $var;?>' <?php if ($_GET['purchase_condition'] == $var) { print 'selected="true"';}else if($_POST['purchase_condition'] == $var){print 'selected="true"'} ?> ><?php echo $var;?></option>
                                         <?php
                                             }
                                         ?>
