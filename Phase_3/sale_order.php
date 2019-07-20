bf2084522ab9ef93002f31bf3d9b5f4d9fb052eb
Jinjun Liu <jliu788@gatech.edu>
2019-07-20 14:14:25 -0500

    bug fix

--- Phase_3/sale_order.php
+++ Phase_3/sale_order.php
@@ -2,28 +2,20 @@
 include('lib/common.php');
 // written by czhang613
 // setup permissino
-if (!isset($_SESSION['username'])) {
-	header('Location: public_search.php');
+if (!isset($_SESSION['username']) OR $_SESSION['permission'] == 1 OR $_SESSION['permission'] == 3) {
+    header('Location: index.php');
 	exit();
-}else {
-    if($_SESSION['permission'] == 3){
-        header("Location: employee_search_manager.php");
-        exit();
-    }
 }
 
 $enteredVIN = $_GET['vin'];
 $enteredcustomer_id = $_GET['customer_id'];
 $enteredsalesperson_permission = 'salesperson_permission_'. $_SESSION['username'];
 
-?>
-
-<?php
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
         $enteredsale_date = mysqli_real_escape_string($db, $_POST['sale_date']);
             $query = "INSERT INTO Sell (vin, customer_id, salesperson_permission, sale_date)"
-                    ."VALUES('$enteredVIN', '$enteredcustomer_id', '$enteredsalesperson_permission', '$enteredsale_date')";
+                    . "VALUES('$enteredVIN', '$enteredcustomer_id', '$enteredsalesperson_permission', '$enteredsale_date')";
             $result = mysqli_query($db, $query);
             include('lib/show_queries.php');
             if (mysqli_affected_rows($db) == -1) {
@@ -50,7 +42,7 @@ $enteredsalesperson_permission = 'salesperson_permission_'. $_SESSION['username'
 
 
 <?php include("lib/header.php"); ?>
-		<title>GTOnline Edit Profile</title>
+		<title>Sale Order</title>
 	</head>
 
 	<body>
@@ -61,7 +53,7 @@ $enteredsalesperson_permission = 'salesperson_permission_'. $_SESSION['username'
 				<div class="center_left">
 					<div class="features">
 
-            <div class="Sale Order section">
+            <div class="profile_section">
 							<div class="subtitle">Sale Order Form</div>
 
                             <form name = "sale_order" action = "sale_order.php?vin=<?php echo $enteredVIN;?>&customer_id=<?php echo $enteredcustomer_id;?>" method="post">
@@ -82,13 +74,13 @@ $enteredsalesperson_permission = 'salesperson_permission_'. $_SESSION['username'
                                             <input name = "sale_date" type = "date" id = "sale_date" value="<?php if ($_POST['sale_date']) { print $_POST['sale_date']; } ?>" >
                                         </td>
                                     </tr>
-                                    <tr>
-                                        <input name = "add" type = "submit" id = "add" value = "Confirm Sale! ">
-                                    </tr>
                                 </table>
+                                <tr>
+                                    <input name = "add" type = "submit" id = "add" value = "Confirm Sale!">
+                                </tr>
                             </form>
-              </div>
-					 </div>
+                    </div>
+					</div>
 				</div>
 
                 <?php include("lib/error.php"); ?>
