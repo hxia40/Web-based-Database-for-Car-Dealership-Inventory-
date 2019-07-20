6e3dba91d6bc72439e0674e1b6f21a2ef6a140c7
Xie, Zijian <jason.xie@gatech.edu>
2019-07-19 23:17:47 -0400

    Add files via upload

--- Phase_3/add_buy.php
+++ Phase_3/add_buy.php
@@ -3,6 +3,9 @@
 include('lib/common.php');
 // written by zxie86
 
+
+
+
 if (!isset($_SESSION['username']) OR ($_SESSION['permission'] != 1 && $_SESSION['permission'] != 4)) {
     header('Location: index.php');
     exit();
